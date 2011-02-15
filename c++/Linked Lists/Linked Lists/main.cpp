d978b4f13047f44c4f87ebc2854319f95ce4f0f9
biga05@gmail.com <biga05@gmail.com@c0f360ae-aefd-11de-b8dd-ab4b6ce6d521>
2011-02-15 22:46:17 +0000

    linked lists completed

--- c++/Linked Lists/Linked Lists/main.cpp
+++ c++/Linked Lists/Linked Lists/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include "node.h"
 #include "list.h"
-#include <string>
+#include <string>\
 #include <ctime>
 
 using namespace std;
@@ -14,65 +14,30 @@ void breakLine()
 
 int main()
 {
-	/* Node Tests 
-	node<string> nAString("test1");
-	node<string> * nodeAPointer = &nAString;
-	node<string> nBString("test2");
-	node<string> * nodeBPointer = &nBString;
-	node<string> nCString("test3");
-	node<string> * nodeCPointer = &nCString;
 
-	nAString.setNext(nodeBPointer);
-	nBString.setPrev(nodeAPointer);
-	nBString.setNext(nodeCPointer);
-	nCString.setPrev(nodeBPointer);
+	list<int> myList;
+	int iInput;
+	cout << "welcome to the josephus solver. Please enter the number of people in the group: ";
+	cin >> iInput;
 
-	cout << "node pointer:" << nodeAPointer << endl;
-	cout << "node data:" << nodeAPointer->getData() << endl;
-	cout << "node next:" << nodeAPointer->getNext() << endl;
-	cout << "node prev:" << nodeAPointer->getPrev() << endl;
-	breakLine();
-	cout << "node pointer:" << nodeBPointer << endl;
-	cout << "node data:" << nodeBPointer->getData() << endl;
-	cout << "node next:" << nodeBPointer->getNext() << endl;
-	cout << "node prev:" << nodeBPointer->getPrev() << endl;
-	breakLine();
-	cout << "node pointer:" << nodeCPointer << endl;
-	cout << "node data:" << nodeCPointer->getData() << endl;
-	cout << "node next:" << nodeCPointer->getNext() << endl;
-	cout << "node prev:" << nodeCPointer->getPrev() << endl;
-	
-	*/
-	list<string> myList;
+	for(int i = 0; i < iInput; i++)
+		myList.append(i);
 
+	for(int i = 0; i < myList.getLength(); i++)
+		cout << myList.get(i) << ", ";
 
-	for (int i = 0; i < 1500 ; i++)
+	cout << endl;
+
+	cout << "And how many people to we skip?: ";
+	cin >> iInput;
+
+	for(int i = iInput-1; myList.getLength() > 1; i += iInput-1)
 	{
-		string nodeData;
-		for (int n = 0 ; n < i ;n++)
-		{
-			nodeData += ".";
-		}
-		myList.push(nodeData);
+		i = i % myList.getLength();
+		cout << "killing " << myList.pop(i) << endl;
 	}
 
-
-	
-	myList.insert("OMG INSERT",5);
-	
-	
-	/*
-	for ( int i = 0; i < myList.getLength() ; i++)
-	{
-		myList.at(i)->print();
-		breakLine();
-	}
-	*/
-	
-	cout << "searching for OMG INSERT returned: " << myList.search("OMG INSERT") << endl;
-	myList.remove(6);
-	cout << "searching for OMG INSERT returned: " << myList.search("OMG INSERT") << endl;
-
+	cout << "last alive: " << myList.pop(0) << endl;
 
 	system("PAUSE");
 }
